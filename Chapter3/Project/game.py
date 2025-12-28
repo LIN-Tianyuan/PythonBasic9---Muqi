d88418182134e3ae2f8dd04d0b48868dc1a4b01d
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2025-12-28 15:56:39 +0100

    week 5

--- Chapter3/Project/game.py
+++ Chapter3/Project/game.py
@@ -1,11 +1,20 @@
 import random
-
+"""
 player = int(input("Que voulez-vous jouer? Pierre(1) / Ciseaux(2) / Papier(3). "))
 computer = 1
 if player == 2:
     print("L'ordinateur gagne.")
 elif player == 3:
-    print("Le joueur gagne.")
+    print("Le player gagne.")
 elif player == 1:
     print("Les deux côtés sont à égalité.")
+"""
 
+player = int(input("Que voulez-vous jouer? Pierre(1) / Ciseaux(2) / Papier(3). "))
+computer = random.randint(1, 3)
+if (player == 3 and computer == 1) or (player == 2 and computer == 3) or (player == 1 and computer == 2):
+    print("Le player gagne. ")
+elif player == computer:
+    print("Les deux côté sont à égalité. ")
+else:
+    print("L'ordinateur gagne.")
---
+++ Chapter4/Course/01_while.py
@@ -0,0 +1,12 @@
+"""
+i = 0
+while i < 100:
+    print("Hello Python")
+    i = i + 1
+"""
+
+a = 0
+while a < 7:
+    a = a + 1
+print(a)
+
